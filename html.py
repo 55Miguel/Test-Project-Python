ad8794db58633650f642085d84695b719a4ff015
Delta55 <danielnurecabral2@gmail.com>
2026-03-25 18:39:38 -0300

    Create html.py

---
+++ html.py
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+        <title>Como acertar a moeda com o tiro no jogo ultrakill lá</title>
+        <link rel="stylesheet" href="style.css">
+  </head>
+<body>
+    <h1>Como acertar a moeda com o tiro no jogo ultrakill lá</h1>
+    <p>Inserir texto...</p>
+    <h2>Você faz esses passos aqui ó</h2>
+             <ul>
+                   <li>Aperta o M2/Mouse direito com a primeira arma do jogo verde lá</li>
+                   <li>mira na moeda e atira</li>
+                   <li>dai se você errar você tenta denovo sabe ai né e tals</li>
+                   <li>dai é só você ficar fazendo isso pronto obrigado tchau me dá dinheiro por favor quero comprar pc gamer com aquelas luz lá</li>
+             </ul>
+     <img src="https://media1.tenor.com/m/aYDOG8YbfQcAAAAd/ultra-kill-ultrakill.gif" alt="Image 1">
+    <iframe 
+        src="https://www.meteoblue.com/en/weather/widget/daily/aracati_brazil_3407357?geoloc=fixed&days=3&tempunit=C&windunit=kmh&precipunit=mm" 
+        frameborder="0" 
+        width="200" 
+        height="300">
+    </iframe>
+</body>
+</html>
